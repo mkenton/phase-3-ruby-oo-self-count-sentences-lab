b31b301e49b1dec6daa97229ffad6adac89f8a51
Michael <mkenton2020@icloud.com>
2021-09-07 03:42:08 -0500

    commit tree 556121dc19bfc22c656097afdc492c55908caaf7

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
-
+  
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/[\.!?] /).count #for complex case, simply follow punctuation with space to allow for splits after "." as well as "!!" .. etc. 
   end
 end
